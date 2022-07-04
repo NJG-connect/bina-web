62aa0823d39cf4fc806e4f8a9ad70bf7d866f7b6
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-04 15:13:34 +0200

    Add : Img & Input components

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,7 @@
-const images: Object = {
+export type ImageType = "user"
 
+const images: { [key in ImageType]: any } = {
+    user: require('./user.png'),
 }
 
 
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,7 @@
 import Title from './Title';
 import Button from './Button';
 import CardButton from './CardButton';
+import Input from './Input';
+import Img from './Img';
 
-export { Title, Button, CardButton };
+export { Title, Button, CardButton, Input, Img };
