58fa89e4dc11945a81360e094016675ece97b581
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-07 11:45:55 +0200

    Add : ImgTemplate component

--- src/components/atoms/ImgTemplate.tsx
+++ src/components/atoms/ImgTemplate.tsx
@@ -3,24 +3,20 @@ import React from "react";
 import images, { ImageType } from "../../assets/images/";
 
 interface Props {
-    img: ImageType;
-    className: string;
-    children: React.ReactNode;
+  img: ImageType;
+  className: string;
+  children: React.ReactNode;
 }
 
-const ImgTemplate: React.FC<Props> = ({img, className, children}) => {
-
-    return (
-
-        <div
-        className={className}
-        style={{ backgroundImage: `url(` + images[img] + `)` }}>
-            {children}
-        </div>
-
-    )
-
-}
-
+const ImgTemplate: React.FC<Props> = ({ img, className, children }) => {
+  return (
+    <div
+      className={className}
+      style={{ backgroundImage: `url(` + images[img] + `)` }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default ImgTemplate;
