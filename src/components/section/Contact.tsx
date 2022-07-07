932c723b7e93d7fbd7f9daac03a68a2bc52136f9
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-07 15:12:21 +0200

    Add : Form verification

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -12,6 +12,21 @@ const Contact: React.FC = () => {
     const [name, setName] = useState<string>("");
     const [tel, setTel] = useState<string>("");
 
+    function isName(name: string) {
+        if(name.trim() != "") return true;
+    }
+
+    function isTel(tel: string) {
+        return tel.match(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/gmi)
+    }
+
+    function verifForm() {
+
+        if(isName(name) && isTel(tel)) console.log(`Form Send : ${name} // ${tel}`)
+
+    }
+
+
     return (
         <section id="contact">
             
@@ -26,7 +41,7 @@ const Contact: React.FC = () => {
                             <Input type="text" onChange={(value) => setName(value)} placeholder="Nom Prénom" icon="user" />
                             <Input type="tel" onChange={(value) => setTel(value)} placeholder="Tél" icon="user" />
                         </div>
-                        <Button value="Envoyer" className="contact-button" onClick={() => console.log(`Form Send : ${name} // ${tel}`)} />
+                        <Button value="Envoyer" className="contact-button" onClick={verifForm} />
                     </div>
 
                 </div>
