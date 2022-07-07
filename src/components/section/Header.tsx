692b67edbc3419a55b89e4a87f98d2c08cabae38
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-07 12:44:49 +0200

    Add : Lyatom compatibilities

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useRef } from "react";
 
 import './header.scss';
+import infoJson from "../../data/data.json";
 import { ImgTemplate } from "../atoms";
+
 
 const Header: React.FC = () => {
 
@@ -10,7 +12,7 @@ const Header: React.FC = () => {
 
         <header>
             <ImgTemplate className="header" htmlId="header" img="header">
-                <h1>Un projet préparé, <br/>Un projet réussi...</h1>
+                <h1 id="header-title">{infoJson.Header.title.line1}<br/>{infoJson.Header.title.line2}</h1>
             </ImgTemplate>
         </header>
 
