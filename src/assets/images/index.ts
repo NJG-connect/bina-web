71d8ec6a8709aa965e012bcb6f788e0d438d8ec8
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-08 13:35:00 +0200

    Add : Icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,6 +4,16 @@ import handleshake from "./handleshake.svg";
 import improvement from "./improvement.svg";
 import key from "./key.svg";
 import sheild from "./sheild.svg";
+import addFile from "./addFile.svg";
+import approve from "./approve.svg";
+import colab from "./colab.svg";
+import corp1 from "./corp1.svg";
+import corp2 from "./corp2.svg";
+import corp3 from "./corp3.svg";
+import corp4 from "./corp4.svg";
+import crossair from "./crossair.svg";
+import key2 from "./key2.svg";
+import like from ".like.svg";
 
 export type ImageType =
   | "user"
@@ -17,21 +27,43 @@ export type ImageType =
   | "menu"
   | "close"
   | "header"
-  | "phone";
+  | "phone"
+  | "addFile"
+  | "approve"
+  | "colab"
+  | "corp1"
+  | "corp2"
+  | "corp3"
+  | "corp4"
+  | "crossair"
+  | "key2"
+  | "like"
+  | "contact";
 
 const images: { [key in ImageType]: any } = {
   user: require("./user.png"),
   letter: require("./letter.png"),
   menu: require("./menu.png"),
   close: require("./close.png"),
-  header: require("./header.jpeg"),
+  header: require("./header.png"),
   phone: require("./phone.png"),
+  contact: require("./contact.png"),
   collaboration,
   delay,
   handleshake,
   improvement,
   key,
   sheild,
+  addFile,
+  approve,
+  colab,
+  corp1,
+  corp2,
+  corp3,
+  corp4,
+  crossair,
+  key2,
+  like,
 };
 
 export default images;
