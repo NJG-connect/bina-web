185097a509fb0a49b5c90a79eaa70abfd5277ae4
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-11 15:35:39 +0200

    Add : Lyatom Compatibilities

--- src/components/section/Presentation.tsx
+++ src/components/section/Presentation.tsx
@@ -4,30 +4,20 @@ import "./presentation.scss";
 import infoJson from "../../data/data.json";
 
 import { Title, Img } from "../atoms";
+import { ImageType } from "../../assets/images";
 
 const Presentation: React.FC = () => {
   return (
     <section id="presentation">
-      <Title value="Notre savoir faire" />
+      <Title value={infoJson.presentation.title} htmlId="presentation-title" />
 
       <div className="content">
         <div className="img-container">
-          <Img img="header" className="img" />
+          <Img img={infoJson.presentation.img as ImageType} className="img" htmlId="presentation-img" />
         </div>
         <div className="text-container">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit
-            amet convallis dolor, vitae pharetra magna. Vivamus ac scelerisque
-            risus. In hac habitasse platea dictumst. Curabitur malesuada id odio
-            et rhoncus. Curabitur non ligula velit. Curabitur tristique tempor
-            accumsan.
-          </p>
-          <p>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-            posuere cubilia curae; Suspendisse justo risus, finibus nec feugiat
-            lobortis, tincidunt vel ante.Suspendisse justo risus, finibus nec
-            feugiat lobortis, tincidunt vel ante.
-          </p>
+          <p id="presentation-p1">{infoJson.presentation.paragraph1}</p>
+          <p id="presentation-p2">{infoJson.presentation.paragraph2}</p>
         </div>
       </div>
     </section>
--- src/data/index.ts
+++ src/data/index.ts
@@ -81,6 +81,43 @@ const data: CmsPropsType = {
             },
           ],
         },
+        //Presentation 
+        {
+          type: "object",
+          title: "Présentation",
+          id: "presentation",
+          fields: [
+            //title
+            {
+              type: "input",
+              title: "Titre",
+              id: "title",
+              htmlId: "presentation-title"
+            },
+            //P1
+            {
+              type: "input",
+              title: "Paragraphe 1",
+              id: "paragraph1",
+              htmlId: "presentation-p1"
+            },
+            //P2
+            {
+              type: "input",
+              title: "Paragraphe 2",
+              id: "paragraph2",
+              htmlId: "presentation-p2"
+            },
+            //Background Image
+            {
+              type: "image",
+              title: "Image",
+              mediaFolder: "src/assets/images",
+              id: "img",
+              htmlId: "presentation-img"
+            },
+          ],
+        },
         //Sponsor 
         {
           type: "object",
