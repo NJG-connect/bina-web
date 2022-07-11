d55a328a7cdd1c53126374c57c4aba6283f8f9f4
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-11 14:28:53 +0200

    Add : Quote component

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -3,5 +3,6 @@ import Header from './Header';
 import Contact from './Contact';
 import Footer from './Footer';
 import Sponsor from './Sponsor';
+import Quote from './Quote';
 
-export { NavBar, Header, Contact, Footer, Sponsor };
+export { NavBar, Header, Contact, Footer, Sponsor, Quote };
