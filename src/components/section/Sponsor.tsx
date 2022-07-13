bfd4367cdc6905fa4ce87841cb01fe7f7e24b624
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-13 12:00:40 +0200

    Fix : Slider on on mobile

--- src/components/section/Sponsor.tsx
+++ src/components/section/Sponsor.tsx
@@ -4,13 +4,20 @@ import "./sponsor.scss";
 import infoJson from "../../data/data.json";
 import { Img, Title } from "../atoms/";
 import { Slider } from "../molecules";
+import useWindowDimensions from "../../utils/windowSizeHook";
 
 const Sponsor: React.FC = () => {
+  const { width } = useWindowDimensions();
+
+  function isSliderShow() {
+    if (infoJson.sponsor.img.length > 4 || width <= 480) return true;
+  }
+
   return (
     <section id="sponsor">
       <Title value={infoJson.sponsor.title} htmlId="sponsor-title" />
 
-      {infoJson.sponsor.img.length > 3 ? (
+      {isSliderShow() ? (
         <Slider
           images={infoJson.sponsor.img.map(
             ({ title }: { title: string }) => title
