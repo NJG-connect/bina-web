04f78c7e41b6d1b95d57bfdc8ae48f76b712335f
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-13 15:35:29 +0200

    Add : Idea

--- src/components/containers/ProjectContainer.tsx
+++ src/components/containers/ProjectContainer.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { ProjectOrganism } from "../organisms/";
+import { Data } from "../../types/Data";
 
 const ProjectContainer: React.FC = () => {
+  const [data, setData] = useState({
+    step: "0",
+  });
 
-    return <ProjectOrganism />;
-}
+  return <ProjectOrganism data={data} setData={setData} />;
+};
 
 export default ProjectContainer;
+
+/*
+
+Context syntax
+{
+    step: string,
+    client: "personal" | "corporation",
+    clientInfo: {
+        corporation?: string,
+        name: string,
+        mail: sting,
+        phone: string (=> "+33 6")
+    },
+    service: "renovation complète" | "investisement locatif" | "rafraîchir et moderniser",
+    mesure: {
+        surfaceTotal: number,
+        room: {
+            bedroom: number,
+            kitchen: number,
+            livingRoom: number,
+            bathroom: number,
+            other: number
+        }
+    },
+    files: file[]
+}
+
+
+*/
--- src/components/organisms/ProjectOrganism.tsx
+++ src/components/organisms/ProjectOrganism.tsx
@@ -1,51 +1,28 @@
 import React from "react";
 
 import infoJson from "../../data/data.json";
+import { Data } from "../../types/Data";
 
-const ProjectOrganism: React.FC = () => {
-  const step: string = "0";
+interface Props {
+  data: Data;
+  setData: React.Dispatch<React.SetStateAction<Data>>;
+}
 
-  switch (step) {
+const ProjectOrganism: React.FC<Props> = ({ data = {}, setData }) => {
+  switch (data.step) {
     case "2":
-      return <h1>Étape 1</h1>;
+      return <h1>Étape 2</h1>;
     case "3":
-      return <h1>Étape 1</h1>;
+      return <h1>Étape 3</h1>;
     case "4":
-      return <h1>Étape 1</h1>;
+      return <h1>Étape 4</h1>;
     case "5":
-      return <h1>Étape 1</h1>;
+      return <h1>Étape 5</h1>;
     default:
-      return <h1>Étape 1</h1>;
+      return (
+        <button onClick={() => setData({ ...data, step: "2" })}>Étape 1</button>
+      );
   }
 };
 
 export default ProjectOrganism;
-
-/*
-
-Context syntax
-{
-    step: string,
-    client: "personal" | "corporation",
-    clientInfo: {
-        corporation?: string,
-        name: string,
-        mail: sting,
-        phone: string (=> "+33 6")
-    },
-    service: "renovation complète" | "investisement locatif" | "rafraîchir et moderniser",
-    mesure: {
-        surfaceTotal: number,
-        room: {
-            bedroom: number,
-            kitchen: number,
-            livingRoom: number,
-            bathroom: number,
-            other: number
-        }
-    },
-    files: file[]
-}
-
-
-*/
