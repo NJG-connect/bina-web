9684fc1f4813403d50869385dfc756ac7b771b61
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-13 12:19:56 +0200

    Fix : Link component (=> key props)

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router";
 
-import './link.scss';
+import "./link.scss";
 
 interface Props {
-    value?: string;
-    href: string;
-    className?: string;
-    link?: string;
-    htmlId?: string;
-    key?: string;
+  value?: string;
+  href: string;
+  className?: string;
+  link?: string;
+  htmlId?: string;
 }
 
-const Link: React.FC<Props> = ({value, href, className = "", link, htmlId = "", key = ""}) => {
+const Link: React.FC<Props> = ({
+  value,
+  href,
+  className = "",
+  link,
+  htmlId = "",
+}) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-
-    return (
-    
-        <a href={href} id={htmlId} key={key} className={`Link ${className}`} onClick={() => link && navigate(link)}>{value}</a>
-        
-    )
-
-}
+  return (
+    <a
+      href={href}
+      id={htmlId}
+      className={`Link ${className}`}
+      onClick={() => link && navigate(link)}
+    >
+      {value}
+    </a>
+  );
+};
 
 export default Link;
