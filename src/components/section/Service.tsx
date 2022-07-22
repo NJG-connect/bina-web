293d578d34044b8fa3bcf8846e6c0a37e45c67e5
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-22 12:08:18 +0200

    Add : Responsive

--- src/components/section/Service.tsx
+++ src/components/section/Service.tsx
@@ -10,7 +10,9 @@ export const Service: React.FC = () => {
     <section id="service">
       <div className="left">
         <div className="top">
-          <IconButton img="arrow.svg" onClick={() => {}} />
+          <div className="arrow">
+            <IconButton img="arrow.svg" onClick={() => {}} />
+          </div>
           <div className="bina-logo"></div>
         </div>
         <h3>Dans quel domaine d'activité êtes-vous ?</h3>
@@ -31,7 +33,7 @@ export const Service: React.FC = () => {
           <ProgressBar step={3} />
         </div>
       </div>
-      <Img img="choose.png" className="right" />
+      <Img img="choose.png" className="right" size="cover" />
     </section>
   );
 };
