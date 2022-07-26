7b4fbaeb591b0091f5b306ca3b064f16402ea1bf
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-26 13:48:58 +0200

    Add : Screen 5

--- src/components/section/Document.tsx
+++ src/components/section/Document.tsx
@@ -23,15 +23,55 @@ const Document: React.FC<Props> = ({ home, back, onSubmit, initialValue }) => {
 
   return (
     <section id="document">
-      <InputFile
-        htmlId="project-screen5-input-title"
-        text="Chargez vos documents"
-        onChange={(file) => setFiles([...files, file])}
-      />
-      {files.length !== 0 &&
-        files.map((elm, index) => (
-          <UploadedFile fileName={elm.name} onRemove={() => remove(index)} />
-        ))}
+      <Img img="choose.png" className="left" size="cover" />
+      <div className="right">
+        <div className="top">
+          <div className="arrow">
+            <IconButton img="arrow.svg" onClick={back} />
+          </div>
+          <div className="bina-logo"></div>
+        </div>
+        <div className="mid">
+          <h3>Déposez vos documents</h3>
+          <InputFile
+            htmlId="project-screen5-input-title"
+            text="Chargez vos documents"
+            onChange={(file) => setFiles([...files, file])}
+          />
+          <div className="file-container">
+            <div
+              className="wrapper"
+              style={
+                files.length == 0
+                  ? {
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }
+                  : {}
+              }
+            >
+              {files.length !== 0 ? (
+                files.map((elm, index) => (
+                  <UploadedFile
+                    fileName={elm.name}
+                    onRemove={() => remove(index)}
+                  />
+                ))
+              ) : (
+                <h1 className="title">Aucun document chargé</h1>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <Button
+            value="Confier mon projet"
+            onClick={() => onSubmit(files)}
+            className="nav-button"
+          />
+        </div>
+      </div>
     </section>
   );
 };
