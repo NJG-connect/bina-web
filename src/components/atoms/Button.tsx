0f027d647bd379d762aca61f02af5311890d516c
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-26 15:36:04 +0200

    Fix : Warnings

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -19,7 +19,7 @@ const Button: React.FC<Props> = ({
   return (
     <div id={htmlId} className={`atom-button ${className}`} onClick={onClick}>
       {value}
-      {className == "submit-button" && <Img className="img" img="check.png" />}
+      {className === "submit-button" && <Img className="img" img="check.png" />}
     </div>
   );
 };
