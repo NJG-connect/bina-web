ee0452dfa126fad49efb6c77feb5cf9c08bd2821
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-07-27 12:33:27 +0200

    Add : Sectioné

--- src/components/section/About.tsx
+++ src/components/section/About.tsx
@@ -3,12 +3,36 @@ import React from "react";
 import infoJson from "../../data/data.json";
 import "./about.scss";
 import { AboutImg } from "../molecules";
+import { Title } from "../atoms";
 
 const About: React.FC = () => {
   return (
     <section id="about">
-      <AboutImg name="Fayçal Benchoira" img="choose.png" htmlId="about-img1" />
-      <AboutImg name="Fayçal Benchoira" img="choose.png" htmlId="about-img1" />
+      <Title value="On fait les présentation" />
+
+      <div className="about-main-container">
+        <div className="left">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit
+            amet convallis dolor, vitae pharetra magna. Vivamus ac scelerisque
+            risus. In hac habitasse platea dictumst. Curabitur malesuada id odio
+            et rhoncus. Curabitur non ligula velit. Curabitur tristique tempor
+            accumsan.
+          </p>
+        </div>
+        <div className="right">
+          <AboutImg
+            name="Fayçal Benchoira"
+            img="choose.png"
+            htmlId="about-img1"
+          />
+          <AboutImg
+            name="Fayçal Benchoira"
+            img="choose.png"
+            htmlId="about-img2"
+          />
+        </div>
+      </div>
     </section>
   );
 };
